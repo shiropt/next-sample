5ec9a195c75b8b733034c33aac00bba990223b93
shiropt <shiro19880610@yahoo.co.jp>
2023-06-06 10:49:59 +0900

    add Button component

---
+++ src/constants/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+  white: "rgb(249, 250, 250)",
+  yellow: "rgb(247, 255, 26)",
+  darkYellow: "rgb(206, 209, 0)",
+  gray: "rgb(61, 69, 73)",
+  darkGray: "rgb(80, 87, 90)",
+  black: "rgb(12, 22, 28)",
+} as const;
