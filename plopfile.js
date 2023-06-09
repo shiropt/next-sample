f92b9d294584ec92dca2b13bae53ef40a844b25a
shiropt <shiro19880610@yahoo.co.jp>
2023-06-09 10:52:34 +0900

    add page template plop

--- plopfile.js
+++ plopfile.js
@@ -12,25 +12,37 @@ const config = (plop) => {
         name: "directory",
         message: "component directory please",
         choices: ["atoms", "molecules", "organisms"],
+        choices: [
+          { name: "app", value: "app" },
+          { name: "atoms", value: "components/atoms" },
+          { name: "molecules", value: "components/molecules" },
+          { name: "organisms", value: "components/organisms" },
+        ],
       },
     ],
-    actions: [
-      {
-        type: "add",
-        path: "src/components/{{directory}}/{{name}}/index.tsx",
-        templateFile: "templates/index.hbs",
-      },
-      {
-        type: "add",
-        path: "src/components/{{directory}}/{{name}}/index.test.tsx",
-        templateFile: "templates/test.hbs",
-      },
-      {
-        type: "add",
-        path: "src/components/{{directory}}/{{name}}/index.stories.tsx",
-        templateFile: "templates/stories.hbs",
-      },
-    ],
+    actions: (data) => {
+      const isApp = data.directory === "app";
+      const file = isApp ? "page" : "index";
+      const templateDirectory = isApp ? "app" : "components";
+      const actions = [
+        {
+          type: "add",
+          path: `src/{{directory}}/{{name}}/${file}.tsx`,
+          templateFile: `templates/${templateDirectory}/index.hbs`,
+        },
+        {
+          type: "add",
+          path: `src/{{directory}}/{{name}}/${file}.test.tsx`,
+          templateFile: `templates/${templateDirectory}/test.hbs`,
+        },
+        {
+          type: `add`,
+          path: `src/{{directory}}/{{name}}/${file}.stories.tsx`,
+          templateFile: `templates/${templateDirectory}/stories.hbs`,
+        },
+      ];
+      return isApp ? actions.splice(0, 1) : actions;
+    },
   });
 };
 module.exports = config;
